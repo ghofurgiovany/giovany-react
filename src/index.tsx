9e64a76e06c0cd9ad5cf55a1c18635ac66937ba7
ghofurgiovany <ahmadghofur2324@gmail.com>
2022-01-12 05:01:13 +0700

    refactor maintsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./pages/App";
 import "@/assets/css/tailwind.css";
 import { AuthProvider } from "./lib";
+
+import App from "./pages/App";
 
 render(
 	<AuthProvider>
